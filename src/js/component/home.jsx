9ba3c62d70a7e29b3d8233eef04804c4df4b4996
Machado123LV <belenmachadolop@gmail.com>
2024-11-11 14:05:13 +0000

    comenzando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    let [listaDeTareas, setListaDeTareas] = useState(["bañarse", "limpiar", "cocinar", "aprender react"])
+    const [nuevaTarea, setNuevaTarea] = useState("")
+
+	const crearUsuario = async ()=> {
+    try {	
+		const response = await fetch ("https://playground.4geeks.com/todo/users/Beli",{
+			method:"POST",
+			headers:{"Content-Type":"application/json"} // estamos creando
+		})
+		if (response.status==201){
+			return true 
+		}
+	} catch (error) {
+		console.log(error); //atrapamos los errores 1
+		return false
+		
+	}
+	}
+
+    const obtenerTareas = async ()=> {
+        try {	
+            const response = await fetch ("https://playground.4geeks.com/todo/users/Beli",{ //revisar URL
+                method:"GET",
+                headers:{"Content-Type":"application/json"} // estamos creando
+            })
+            if (response.status==404){
+                crearUsuario()
+                return true 
+            }
+        } catch (error) {
+            console.log(error); //atrapamos los errores
+            return false
+            
+        }
+        }
+
+    const agregarTarea = (evento) => {
+        evento.preventDefault()
+        if (nuevaTarea.trim() !== "") {
+            setListaDeTareas([...listaDeTareas, nuevaTarea])
+            setNuevaTarea("")
+        }
+    }
+
+    const eventoEnter = (evento) => {
+        if (evento.key === "Enter") {
+            agregarTarea(evento)
+        }
+    }
+
+    const borrarTarea = (evento, index) => {
+        evento.preventDefault()
+        let listaFiltrada = listaDeTareas.filter((task, i) => {
+            return (i != index)
+        })
+        setListaDeTareas(listaFiltrada)
+    }
+
+    return (
+        <div className="container mt-5">
+            <h1 className="text-center mt-5">Todos</h1>
+
+            <div className="mx-auto col-6">
+                <input type="text"
+                    className="form-control"
+                    placeholder="¿Que Necesitas Hacer?"
+                    value={nuevaTarea}
+                    onChange={(evento) => setNuevaTarea(evento.target.value)}
+                    onKeyDown={eventoEnter}
+                />
+
+                {/* <div className="d-flex justify-content-center">
+                    <button className="btn btn-outline-success mt-3 mb-3 w-50"
+                        onClick={(evento) => agregarTarea(evento)}>
+                        Agregar Tarea
+                    </button>
+                </div> */}
+
+                <ul className="list-group">
+                    {listaDeTareas.map((item, index) => {
+                        return (
+                            <li className="list-group-item" key={index}>
+                                {item}  <i className="fa-solid fa-trash icono-oculto float-end fs-4"
+                                    onClick={(evento) => borrarTarea(evento, index)}>
+                                </i>
+                            </li>
+                        )
+                    })}
+
+                </ul>
+
+
+            </div>
+        </div>
+
+    );
 };
 
 export default Home;
